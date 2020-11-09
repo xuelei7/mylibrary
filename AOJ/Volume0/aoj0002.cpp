74b5359f412f51de9ed45da25ce8d8b763a47bad
xuelei7 <yuxuelei52@hotmail.com>
2020-11-09 17:17:11 +0900

    aoj0002, aoj0003

---
+++ AOJ/Volume0/aoj0002.cpp
@@ -0,0 +1,32 @@
+// Digit Number
+// Write a program which computes the digit number of sum of two integers a and b.
+
+// Input
+// There are several test cases. Each test case consists of two non-negative integers a and b which are separeted by a space in a line. The input terminates with EOF.
+
+// Constraints
+// 0 ≤ a, b ≤ 1,000,000
+// The number of datasets ≤ 200
+// Output
+// Print the number of digits of a + b for each data set.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b;
+    while (cin >> a >> b) {
+        int c = a + b;
+        if (c == 0) {
+            cout << 1 << endl;
+            continue;
+        }
+        int cnt = 0;
+        while (c > 0) {
+            c /= 10;
+            cnt++;
+        }
+        cout << cnt << endl;
+    }
+
+    return 0;
+}
---
+++ AOJ/Volume0/aoj0003.cpp
@@ -0,0 +1,28 @@
+// Is it a Right Triangle?
+// Write a program which judges wheather given length of three side form a right triangle. Print "YES" if the given sides (integers) form a right triangle, "NO" if not so.
+
+// Input
+// Input consists of several data sets. In the first line, the number of data set, N is given. Then, N lines follow, each line corresponds to a data set. A data set consists of three integers separated by a single space.
+
+// Constraints
+// 1 ≤ length of the side ≤ 1,000
+// N ≤ 1,000
+// Output
+// For each data set, print "YES" or "NO".
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    while (n--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (b > a) swap(a,b);
+        if (c > a) swap(a,c);
+        if (a * a == b * b + c * c) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+    return 0;
+}
