f725d93de404111b6698f6e71596104566039eff
xuelei7 <yuxuelei52@hotmail.com>
2020-11-22 00:38:35 +0900

    added geometry class - Point & contains function

--- geometry.cpp
+++ geometry.cpp
@@ -26,3 +26,58 @@ pair<pair<double,double>, double> CircumscribedCircleOfATriangle(double x1, doub
     r = sqrt((x1 - px) * (x1 - px) + (y1 - py) * (y1 - py));
     return {{px,py},r};
 }
+
+// from
+// aoj0012
+// returns if a polygon contains a point
+#define EPS (1e-10)
+class Point{
+public:
+  double x,y;
+  Point(double x = 0, double y = 0):x(x), y(y) {}
+  Point operator + (Point p){
+    return Point(x+p.x,y+p.y);
+  }
+  Point operator - (Point p){
+    return Point(x-p.x,y-p.y);
+  }
+  Point operator * (double k){
+    return Point(x*k,y*k);
+  }
+  Point operator / (double k){
+    return Point(x/k,y/k);
+  }
+  bool operator == (const Point &p) const{
+    return fabs(x-p.x) < EPS && fabs(y-p.y) < EPS;
+  }
+};
+typedef Point Vector;
+typedef vector<Point> Polygon;
+double dot(Vector a,Vector b){
+  return a.x * b.x + a.y * b.y;
+}
+double cross(Vector a,Vector b){
+  return a.x * b.y - a.y * b.x;
+}
+// IN 2 ON 1 OUT 0
+int contains(Polygon g,Point p){
+  int n=g.size();
+  bool x = false;
+  for(int i=0;i<n;i++){
+    Point a = g[i] - p, b = g[(i+1)%n] - p;
+    if(fabs(cross(a,b))<EPS && dot(a,b)<EPS) return 1;
+    if(a.y > b.y) swap(a,b);
+    if(a.y<EPS && EPS < b.y && cross(a,b) > EPS) x = !x;
+  }
+  return ( x ? 2:0);
+}
+
+// from
+// aoj0012
+// return if the triangle contains a point
+bool trangleContainsPoint(double x1, double y1, double x2, double y2, double x3, double y3, double xp, double yp) {
+    double c1 = (x2 - x1) * (yp - y1) - (y2 - y1) * (xp - x1);
+    double c2 = (x3 - x2) * (yp - y2) - (y3 - y2) * (xp - x2);
+    double c3 = (x1 - x3) * (yp - y3) - (y1 - y3) * (xp - x3);
+    return ((c1>0 && c2>0 && c3>0) || (c1<0 && c2<0 && c3<0));
+}
