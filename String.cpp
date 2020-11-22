00b418ffee00dd6e7154c443961e0590ff645b2c
xuelei7 <yuxuelei52@hotmail.com>
2020-11-22 23:04:45 +0900

    add toUpper function to string

--- String.cpp
+++ String.cpp
@@ -41,3 +41,12 @@ vector<string> split_string(string s, char c) {
     }
     return v;
 }
+
+// from
+// aoj0020
+string toUpper(string s) {
+    for (size_t i = 0; i < s.size(); i++) {
+        if (s[i] >= 'a' && s[i] <= 'z') s[i] = toupper(s[i]);
+    }
+    return s;
+}
