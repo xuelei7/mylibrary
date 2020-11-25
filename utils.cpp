44e97688d7329082b18276cc93f633b0e1b470a4
xuelei7 <yuxuelei52@hotmail.com>
2020-11-25 13:09:26 +0900

    add Zeller function - calculate the date from year/month/day

--- utils.cpp
+++ utils.cpp
@@ -10,3 +10,15 @@ int digitnum(int k) {
     }
     return cnt;
 }
+
+// from
+// aoj0027
+// return the date of year/month/day
+// 0: Sunday 1: Monday ...
+int Zeller(int year, int month, int day) {
+    if (month == 1 || month == 2) {
+        year--;
+        month += 12;
+    }
+    return (year + year/4 - year/100 + year/400 + (13*month+8)/5 + day) % 7;
+}
