cc37ef6677f7408bb6aa4b6d7fbb85fc64b83660
xuelei7 <yuxuelei52@hotmail.com>
2021-01-29 11:39:17 +0900

    modificate library - utils/dijkstra

--- utils.cpp
+++ utils.cpp
@@ -227,6 +227,38 @@ void WarshallFloyd() {
     }
 }
 
+//------------------------------
+// from
+// aoj0156
+typedef pair<int,int> P;
+struct edge{int to, cost;};
+const int INF = 1e9;
+int V;
+const int MAX_V = 1e5;
+vector<edge> G[MAX_V];
+int d[MAX_V];
+
+void dijkstra(int s, int *d, vector<edge> *G, int V) {
+  priority_queue<P, vector<P>, greater<P>> que;
+  fill(d, d + V, INF);
+  d[s] = 0;
+  que.push(P(0, s));
+
+  while (!que.empty()) {
+    P p = que.top();
+    que.pop();
+    int v = p.second;
+    if (d[v] < p.first) continue;
+    for (int i = 0; i < G[v].size(); i++) {
+      edge e = G[v][i];
+      if (d[e.to] > d[v] + e.cost) {
+        d[e.to] = d[v] + e.cost;
+        que.push(P(d[e.to], e.to));
+      }
+    }
+  }
+}
+
 
 // menu
 // int digitnum(int k)
@@ -240,3 +272,4 @@ void WarshallFloyd() {
 // union find
 // int count_days(int y, int m, int d)
 // void WarshallFloyd
+// void dijkstra
