bf656e8af18dc52ff3658068d4a90249face9287
xuelei7 <yuxuelei52@hotmail.com>
2021-03-02 13:03:09 +0900

    added template

--- AOJ/Volume25(JAG)/aoj2566_RestoreCalculation.cpp
+++ AOJ/Volume25(JAG)/aoj2566_RestoreCalculation.cpp
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-// Problem Statement
-// The Animal School is a primary school for animal children. You are a fox attending this school.
-
-// One day, you are given a problem called "Arithmetical Restorations" from the rabbit teacher, Hanako. Arithmetical Restorations are the problems like the following:
-
-// You are given three positive integers, [Math Processing Error], [Math Processing Error] and [Math Processing Error].
-
-// Several digits in these numbers have been erased.
-
-// You should assign a digit in each blank position so that the numbers satisfy the formula [Math Processing Error].
-
-// The first digit of each number must not be zero. It is also the same for single-digit numbers.
-
-// You are clever in mathematics, so you immediately solved this problem. Furthermore, you decided to think of a more difficult problem, to calculate the number of possible assignments to the given Arithmetical Restorations problem. If you can solve this difficult problem, you will get a good grade.
-
-// Shortly after beginning the new task, you noticed that there may be too many possible assignments to enumerate by hand. So, being the best programmer in the school as well, you are now trying to write a program to count the number of possible assignments to Arithmetical Restoration problems.
-
-// Input
-// The input is a sequence of datasets. The number of datasets is less than 100. Each dataset is formatted as follows.
-
-// [Math Processing Error]
-// [Math Processing Error]
-// [Math Processing Error]
-// Each dataset consists of three strings, [Math Processing Error], [Math Processing Error] and [Math Processing Error]. They indicate that the sum of [Math Processing Error] and [Math Processing Error] should be [Math Processing Error]. Each string consists of digits (0-9) and/or question mark (?). A question mark (?) indicates an erased digit. You may assume that the first character of each string is not 0 and each dataset has at least one ?.
-
-// It is guaranteed that each string contains between 1 and 50 characters, inclusive. You can also assume that the lengths of three strings are equal.
-
-// The end of input is indicated by a line with a single zero.
-
-// Output
-// For each dataset, output the number of possible assignments to the given problem modulo 1,000,000,007. Note that there may be no way to solve the given problems because Ms. Hanako is a careless rabbit.
-
-=======
 // Problem Statement
 // The Animal School is a primary school for animal children. You are a fox attending this school.
 
